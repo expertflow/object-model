eca53553714a28d9d38cdea4fe42110a457ec783
ahmad-bappi <ahmad.bappi@expertflow.com>
2022-09-04 23:17:56 +0500

    ActionMessageBody added

--- src/main/java/com/ef/cim/objectmodel/MessageBody.java
+++ src/main/java/com/ef/cim/objectmodel/MessageBody.java
@@ -21,6 +21,7 @@ import javax.validation.constraints.NotNull;
         @Type(value = UrlMessage.class, name = "URL"), @Type(value = StickerMessage.class, name = "STICKER"),
         @Type(value = ReceiptTemplateMessage.class, name = "RECEIPT"),@Type(value = WrapUpMessage.class, name = "WRAPUP"),
         @Type(value = TemplateMessage.class, name = "TEMPLATE"),
+        @Type(value = ActionMessageBody.class, name = "ACTION"),
         @Type(value = CommentMessage.class, name = "COMMENT")})
 public class MessageBody implements Serializable {
     @NotNull
