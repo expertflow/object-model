84eb8033961d2bea6bfae0497bd07324bfef3df4
AamnaZahid <amna.zahid@expertflow.com>
2024-10-23 10:06:27 +0500

    Formdata message type added. Sentiment class added

---
+++ src/main/java/com/ef/cim/objectmodel/FormDataMessage.java
@@ -0,0 +1,16 @@
+package com.ef.cim.objectmodel;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.Valid;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FormDataMessage extends MessageBody{
+    @Valid
+    private FormData formData;
+}
---
+++ src/main/java/com/ef/cim/objectmodel/Sentiment.java
@@ -0,0 +1,11 @@
+package com.ef.cim.objectmodel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Sentiment {
+    private String result;
+    private String color;
+}
