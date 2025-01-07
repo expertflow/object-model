26600d6b2bd38cf6376ee9cfed8e5eb021cf4e04
amna <amna.zahid@expertflow.com>
2025-01-07 16:08:11 +0500

    Changes added in SchedulingMetaData.java

--- src/main/java/com/ef/cim/objectmodel/SchedulingMetaData.java
+++ src/main/java/com/ef/cim/objectmodel/SchedulingMetaData.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
@@ -12,38 +13,5 @@ import java.sql.Timestamp;
 public class SchedulingMetaData {
     private Timestamp scheduledDatetime;
     private String webhookId;
-    private String agent;
-    private String queue;
-    private String ivr;
-    private String gatewayId;
-    private String campaignId;
-    private String campaignContactId;
-    private String campaignType;
-    private String startTime;
-    private String endTime;
-    private String priority;
-    private DialingMode dialingMode; // PREDICTIVE, PREVIEW, or PROGRESSIVE
-    private String routingMode; // AGENT or QUEUE
-    private String resourceId;
-    private String queueName;
-    private ChannelTypeDTO channelType;
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ChannelTypeDTO {
-        private String name;
-        private String channelLogo;
-    }
-
-    public enum DialingMode {
-        PREDICTIVE,
-        PREVIEW,
-        PROGRESSIVE
-    }
-
-    public enum RoutingMode {
-        AGENT,
-        QUEUE
-    }
+    private Map<String, Object> metadata;
 }
