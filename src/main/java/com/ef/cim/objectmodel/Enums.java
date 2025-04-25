b63378ab344abd8cb951c8963d6bc00f9761cb77
Ahsan Jalil <ahsan.jalil@expertflow.com>
2025-04-25 15:38:59 +0500

    New Task State Reason Code Added for OM 1.22.7

--- src/main/java/com/ef/cim/objectmodel/Enums.java
+++ src/main/java/com/ef/cim/objectmodel/Enums.java
@@ -137,7 +137,19 @@ public class Enums {
         /**
          * External Direct Transfer task state reason code.
          */
-        EXTERNAL_DIRECT_TRANSFER
+        EXTERNAL_DIRECT_TRANSFER,
+        /**
+         * External Consult Conference task state reason code.
+         */
+        EXTERNAL_CONSULT_CONFERENCE,
+        /**
+         * Conference Ended task state reason code.
+         */
+        CONFERENCE_ENDED,
+        /**
+         * Conference Customer Left task state reason code.
+         */
+        CONFERENCE_CUSTOMER_LEFT
     }
 
     /**
